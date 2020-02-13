8c6964ab86683f32e376fd163f94f6e233608106
JakeRivett31 <jake.rivett@ucc.on.ca>
2020-02-13 09:13:12 -0500

    nav bar and parallax

---
+++ scriptfeb7.js
@@ -0,0 +1,14 @@
+for (i = 0; i < 100; i = i + 1) {
+
+	console.log(i)
+}
+
+list = [45,5,6,45,45,45,56,23,234,42,4343,43,43,2,6,5,7,7,9,89,45,33,4]
+
+sum = 0
+
+for (i = 0; i < list.length; i = i + 1) {
+	sum = sum + list[i];
+}
+
+console.log(sum)
